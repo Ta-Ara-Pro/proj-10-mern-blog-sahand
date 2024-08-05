900f276e2b8837114db3cb37f9b2a56dcfcf79df
Ta-Ara-Pro <tahereh.q.ara@gmail.com>
2024-08-05 15:16:27 +0330

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose
+    .connect(
+        process.env.MONGO_URI
+    ).then(() => { 
+        console.log('Mongodb is connected')
+    }).catch((err) => {
+        console.log({'Mongodb connection error': err})
+    })
 
 const app = express(); //create the application
 
 app.listen(3000, () => {
     console.log('Server is running at port 3000')
 })
+   
