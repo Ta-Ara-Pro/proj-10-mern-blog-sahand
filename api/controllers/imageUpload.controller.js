0a2669774fbd6e7cb35dabb04715c81023164148
Ta-Ara-Pro <tahereh.q.ara@gmail.com>
2024-12-06 20:15:39 +0330

    complete profile image upload functionality

---
+++ api/controllers/imageUpload.controller.js
@@ -0,0 +1,32 @@
+import cloudinary from '../utils/cloudinary.js';
+import upload from '../utils/multer.js';
+
+// Middleware for file handling
+export const uploadMiddleware = upload.single('file');
+
+// Controller to handle Cloudinary uploads
+export const ImageUploader = async (req, res, next) => {
+  try {
+    // Check if a file exists in the request
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded!' });
+    }
+
+    // Upload to Cloudinary
+    const uniqueFilename = req.body.public_id; // Use the filename generated on the frontend
+    
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: 'user-profile-image', // Optional: Organize files in folders
+      public_id: uniqueFilename,
+    });
+
+    // Respond with the uploaded image URL
+    res.status(200).json({
+      message: 'Image uploaded successfully!',
+      url: result.secure_url, // URL for the uploaded image
+    });
+  } catch (error) {
+    console.error('Cloudinary Upload Error:', error.message);
+    res.status(500).json({ error: 'Failed to upload image', details: error.message });
+  }
+};
