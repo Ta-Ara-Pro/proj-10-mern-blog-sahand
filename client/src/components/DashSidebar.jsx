f93a88ab8eb9d551ac296b238b623f730d03992f
Ta-Ara-Pro <tahereh.q.ara@gmail.com>
2024-12-16 19:42:38 +0000

    add dashboard overview to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Sidebar } from 'flowbite-react'
-import { HiAnnotation, HiArrowSmRight, HiOutlineDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
+import { HiAnnotation, HiArrowSmRight, HiChartPie, HiHome, HiOutlineDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
 import { Link, useLocation } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux'
@@ -43,6 +43,18 @@ const DashSidebar = () => {
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
         <Sidebar.ItemGroup className='flex flex-col gap-1'>
+          {currentUser?.isAdmin &&
+          <Link to='/dashboard?tab=dash'>
+          <Sidebar.Items
+          active={tab === 'dash' || !tab}
+          icon={HiChartPie}
+          as='div'
+          >
+            Dashboard
+          </Sidebar.Items>
+          </Link>
+          }
+
           <Link to='/dashboard?tab=profile'>
             <Sidebar.Item active={tab === 'profile'} icon={HiUser}
               label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark' as='div'>
