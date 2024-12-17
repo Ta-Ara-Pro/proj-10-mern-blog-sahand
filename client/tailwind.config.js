ed3171603a5da155c6747602c46cde6a27a2d1c2
Ta-Ara-Pro <tahereh.q.ara@gmail.com>
2024-12-17 20:17:23 +0000

    fix building issues-3-change tailwind.config and add .vercel file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -18,20 +18,39 @@
 // }
 
 
-/** @type {import('tailwindcss').Config} */
-export default {
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     "./node_modules/flowbite-react/**/*.js", // For flowbite-react components
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require("tailwind-scrollbar"),
+//     // require("@tailwindcss/line-clamp"),
+//   ],
+// };
+
+
+import { defineConfig } from "tailwindcss";
+import tailwindScrollbar from "tailwind-scrollbar";
+import flowbite from "flowbite-react/tailwind";
+
+export default defineConfig({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite-react/**/*.js", // For flowbite-react components
+    "./node_modules/flowbite-react/**/*.js",
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require("tailwind-scrollbar"),
-    // require("@tailwindcss/line-clamp"),
+    flowbite,
+    tailwindScrollbar,
+    // Add other plugins here if needed
   ],
-};
-
-
+});
