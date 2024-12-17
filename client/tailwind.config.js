6c69fdf466e619c7234ab80677c0d461b790ba6e
Ta-Ara-Pro <tahereh.q.ara@gmail.com>
2024-12-17 20:52:34 +0000

    fix build issues-4

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -18,39 +18,21 @@
 // }
 
 
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//     "./node_modules/flowbite-react/**/*.js", // For flowbite-react components
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [
-//     require("tailwind-scrollbar"),
-//     // require("@tailwindcss/line-clamp"),
-//   ],
-// };
-
-
-import { defineConfig } from "tailwindcss";
-import tailwindScrollbar from "tailwind-scrollbar";
-import flowbite from "flowbite-react/tailwind";
-
-export default defineConfig({
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite-react/**/*.js",
+    "./node_modules/flowbite-react/**/*.js", // For flowbite-react components
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    flowbite,
-    tailwindScrollbar,
-    // Add other plugins here if needed
+    require("tailwind-scrollbar"),
+    // require("@tailwindcss/line-clamp"),
   ],
-});
+};
+
+
+
